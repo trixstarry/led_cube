7ef7d2edb22e0e85d43e02c333f83cbcfeebed8c
Alex Dziggel <ddziggel@gmail.com>
2013-02-04 13:28:37 -0800

    commented out communication.c. It is now ready to fill out.

--- communication.c
+++ communication.c
@@ -5,38 +5,68 @@
 //
 // Communication.c contains the low level communication methods and protocols for communicating between the computer and a micro controller via USART.
 //
+// Send Data
+// Receive ACK/NACK
+// ACK: Send next data     NACK: Resend Data
+//
+// Send Check Command   - checks hall effect sensors
+//
+// Broadcast to display frame X
+//
+// PACKET:
+// START BYTE - 8 bits  - 1 byte
+// TO         - 4 bits
+// FROM       - 4 bits  - 1 bytes
+// SUBJECT    - 3 bits
+// LENGTH     - 4 bits  - 1 bytes        Possible stuffing
+// Frame #    - 9 bits  - 1 byte         Possible Stuffing
+// Packet #   - 3 bits  - 
+// DATA       - 69 bits - 9 bytes       Possible to get up to twice the bytes here with stuffing
+// CHECKSUM   - 16 bits -  2 bytes       Possible to get up to 4 bytes
+// END BYTE   - 8 bits  - 1 byte
+//
 // This is the uC's communication file
 
 // NOTE: Protocol and ID defines are randomly generated.
 // PROTOCOLS:
-#define RECEIVE_READY   0x26
-#define TRANSMIT_READY  0x9E
-#define ACK             0xEA
-#define NACK            0x85
-#define TRANSMIT_DONE   0xFB
+#define START_BYTE      0x01
+#define END_BYTE        0xFF
+#define DATA            0x03
+#define ACK             0x04
+#define NACK            0x05
+#define STUFF_BYTE      0x02
 
 //IDs:
-#define ID_SELF         0xE3 
-#define ID_COMP         0x03
-#define ID_CUBE1        0x54
-#define ID_CUBE2        0x17
-#define ID_CUBE3        0x23
-#define ID_CUBE4        0xDB
-#define BROADCAST       0xF6
+#define ID_SELF         0x03 
+#define ID_COMP         0x04
+#define ID_CUBE1        0x05
+#define ID_CUBE2        0x06
+#define ID_CUBE3        0x07
+#define ID_CUBE4        0x08
+#define BROADCAST       0x09
 
 
-#define TIMEOUT         5   // in milliseconds
+#define TIMEOUT         40   // in milliseconds
 
-uint8_t parse_packet(uint64_t packet){
-    static uint8_t FROM = 0; 
-    static uint8_t TO = 0;
-    static uint8_t SUBJ = 0;
-    static uint8_t packet = 0;
-    static uint32_t msg = 0;
+uint8_t parse_packet(uint32_t packet){
+    // check ID otherwise ignore
+    // destuff
+    // check checksum otherwise nack
+    // get subject
+    // store who its from incase a response is needed
+    // get data
+    // return everything
+}
 
-    FROM = ((header & 0xE0)>>5);
-    TO = ((header & 0x1C)>>2);
-    SUBJ = (header & 0x03)
+int8_t create_packet(uint8_t to, uint8_t subject, uint16_t frame,uint64_t *data, uint32_t *output){
+    //for data in len of data
+    //      break data into 9 byte increments
+
+    //for data in list of data
+    //      output[i] = startbyte + all that stuff listted in the header
+
+    // return success otherwise return -1
+
 }
 
 uint8_t calculate_checksum(uint8_t *data, uint8_t len){
@@ -45,41 +75,96 @@ uint8_t calculate_checksum(uint8_t *data, uint8_t len){
     uint8_t checksum;
     uint16_t sum =0;
 
-    /*
-     * Our algorithm is simple, using a 32 bit accumulator (sum),
-     * we add sequential 8 bit words to it, and at the end, fold
-     * back all the carry bits from the top 8 bits into the lower
-     * 8 bits.
-     *
-     */
-    while (ileft > 1)  {
+    while(ileft > 1){
         sum += *i++;
         ileft -= 2;
     }
 
-    /* mop up an odd byte, if necessary */
-    if (ileft == 1)
-        sum += *(uint8_t *)i;
-
-    /*
-     * add back carry outs from top 8 bits to low 8 bits
-     *
-     */
-    sum = (sum >> 8) + (sum & 0xffff);  /* add hi 8 to low 8 */
-    sum += (sum >> 8);                  /* add carry */
-    checksum = ~sum;                    /* truncate to 8 bits */
     return (checksum);
+
 }
 
-uint8_t check_receive()
+/*
+uint8_t calculate_checksum(uint8_t *data, uint8_t len){
+    uint8_t ileft = len;
+    uint8_t *i = data;
+    uint8_t checksum;
+    uint16_t sum =0;
 
-
-    uint8_t transmit(uint8_t id, uint8_t type, uint8_t data){
-        USART(id)
-            send(TRANSMIT_READY)
-
-
+    while (ileft > 1)  {
+        sum ^= *i++;
+        ileft -= 2;
     }
+
+    // mop up an odd byte, if necessary 
+    if (ileft == 1){
+        sum ^= *(uint8_t *)i;
+    }
+
+    checksum = ~sum;                    // truncate to 8 bits 
+    return (checksum);
+}
+*/
+
+uint8_t destuffer(uint8_t data){
+    if(data == START_BYTE){
+        //do stuff
+        //return stuff
+    }
+    if(data == STOP_BYTE){
+        //do stuff
+        //return stuff
+    }
+    if(data == STUFF_BYTE){
+        //do stuff 
+        //return stuff
+    }
+}
+
+
+/* step before transmitting usart
+ */
+uint8_t stuffer(uint8_t data){
+    if(data == START_BYTE){
+        //do stuff
+        //return stuff
+    }
+    if(data == STOP_BYTE){
+        //do stuff
+        //return stuff
+    }
+    if(data == STUFF_BYTE){
+        //do stuff 
+        //return stuff
+    }
+}
+
+//Transmit sends data to the specified location and returns 
+// the value of the cubes touching it
+uint8_t transmit(uint8_t id, uint8_t type, uint8_t data){
+    // Break data into packets
+
+    // load data into buffer of bytes
+
+    // while (there is a packet): 
+    //      send packet   - Stuff while sending packet
+    //      wait for ack
+    //      if ack        if nack
+    //      check ack     do nothing
+    //      store data
+    //      increment packet counter
+
+}
+
+
+//
+//
+uint8_t receive( void ){
+    // parse packet
+    // ACK
+
+
+}
 
 
 
