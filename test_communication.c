2fb83e5bd63b873200210a78c0f9395ced3401a7
Valerie Hoang <thaotrang.hoang@gmail.com>
2013-02-13 18:27:03 -0800

    small update

--- test_communication.c
+++ test_communication.c
@@ -15,7 +15,11 @@ extern int8_t create_packets(uint8_t to, uint8_t subject, uint16_t frame, uint8_
 
 test_create_packets(){
     uint8_t data[10] = {0,0,0,0,0,0,0,0,0,0};
-
+    uint8_t to = 0;
+    uint8_t subject = 0;
+    uint16_t frame = 0;
+    uint8_t data_len = 0;
+    uint64_t packets[][] = { {0} };
     //check length algorithm
     length = sizeof(data)/sizeof(data[0]);
 
