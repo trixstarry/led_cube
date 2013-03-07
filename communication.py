afdba55a450518c303c4c5b77346ca25c3650fcc
Alex Dziggel <ddziggel@gmail.com>
2013-03-06 20:07:37 -0800

    looking at GUI code and thinking about how it should function.

--- communication.py
+++ communication.py
@@ -4,7 +4,24 @@ import serial
 
 class Communication():
     def __init__(self):
-        self.s1 = serial.Serial('/dev/ttyUSB0')
+        try:
+            self.s1 = serial.Serial('/dev/ttyUSB0')
+        except serial.serialutil.SerialException:
+            print "ERROR: Could not open serial Connection on USB0"
+            try:
+                self.s1 = serial.Serial('/dev/ttyUSB1')
+            except serial.serialutil.SerialException:
+                print "ERROR: Could not open serial Connection on USB1"
+                try:
+                    self.s1 = serial.Serial('/dev/ttyUSB2')
+                except serial.serialutil.SerialException:
+                    print "ERROR: Could not open serial Connection on USB2"
+                    try:
+                        self.s1 = serial.Serial('/dev/ttyUSB3')
+                    except serial.serialutil.SerialException:
+                        print "ERROR: Could not open serial Connection on USB3"
+                        print "ERROR: Could not find LEDCUBE USB Dongle."
+
 
     def Transmit(self,data):
         self.s1.write(data)
