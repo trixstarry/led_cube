90c1fc7de64f025e7f679f63694f6ea3f115a961
Alex Dziggel <ddziggel@gmail.com>
2013-04-21 14:55:02 -0700

    changed channel to 2. may have forgotten to change cube ID originally

--- mirf.h
+++ mirf.h
@@ -30,7 +30,7 @@
 #include <avr/io.h>
 
 // Mirf settings
-#define mirf_CH         5
+#define mirf_CH         2
 #define mirf_PAYLOAD    32
 #define mirf_CONFIG     ( (1<<MASK_RX_DR) | (1<<EN_CRC) | (0<<CRCO) )
 
